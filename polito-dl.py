acf1c523dccfc6f2512a8d9c4a7c97af1385f8a9
Giuseppe <gius-italy@live.it>
2017-12-06 22:24:45 +0100

    Script rewritten in a better way, still to improve

---
+++ polito-dl.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+
+#Polito Videolessons Downloader 
+
+import os,requests,urllib,re,html,sys
+
+
+def polito_login(user,passw):
+    with requests.session() as s :
+        r=s.get('https://idp.polito.it/idp/x509mixed-login')
+        r=s.post('https://idp.polito.it/idp/Authn/X509Mixed/UserPasswordLogin',data={'j_username':user,'j_password':passw})
+        relaystate=html.unescape(re.findall('name="RelayState".*value="(.*)"',r.text)[0])
+        samlresponse=html.unescape(re.findall('name="SAMLResponse".*value="(.*)"',r.text)[0])
+        r=s.post('https://www.polito.it/Shibboleth.sso/SAML2/POST',data={'RelayState':relaystate,'SAMLResponse':samlresponse})
+        r=s.post('https://login.didattica.polito.it/secure/ShibLogin.php')
+        relaystate=html.unescape(re.findall('name="RelayState".*value="(.*)"',r.text)[0])
+        samlresponse=html.unescape(re.findall('name="SAMLResponse".*value="(.*)"',r.text)[0])
+        r=s.post('https://login.didattica.polito.it/Shibboleth.sso/SAML2/POST',data={'RelayState':relaystate,'SAMLResponse':samlresponse})
+        if r.url=="https://didattica.polito.it/portal/page/portal/home/Studente": #Login Successful
+            return s.cookies
+        else:
+            print("Something went wrong with the login...")
+            return ""
+
+
+def extract_video_links(url,login_cookie):
+    with requests.session() as s:
+        s=requests.session()
+        s.cookies=login_cookie
+        r=s.get(url) 
+    #Different html structure for videolessons on elearning.polito.it and didattica.polito.it
+    if "didattica.polito.it" in url:
+        links=re.findall('href="(sviluppo\.videolezioni\.vis.*lez=\w*)">',r.text)
+        for i in range(len(links)):
+            links[i]='https://didattica.polito.it/pls/portal30/'+html.unescape(links[i])
+        return links
+    else:
+        print("Sorry, still under developement")
+        return ""
+
+
+def download_video(url,login_cookie,directory):
+    with requests.session() as s:
+        s=requests.session()
+        s.cookies=login_cookie
+        if "didattica.polito.it" in url:
+            r=s.get(url)
+            d_url=re.findall('href="(.*)".*Video',r.text)[0]
+            r=s.get('https://didattica.polito.it'+html.unescape(d_url),allow_redirects=False)
+            d_url=r.headers['location']
+            filename=d_url.split('/')[-1]
+            print('Download in corso di "'+filename+'"')
+            urllib.request.urlretrieve(d_url,os.path.join(directory,filename))
+        else:
+            print("Sorry, still under developement")
+            return ""
+
+    
+    
+    
+    
+
+
+
+try:
+    directory=os.path.dirname(os.path.realpath(__file__))
+except:
+    directory=os.getcwd()
+    
+
+if len(sys.argv)<2:
+    print("Interactive Mode")
+    user=input("Inserisci il tuo nome utente del politecnico: ")
+    passw=input("Inserisci la tua password: ")
+    video_url=input("Inserisci il link delle videolezioni: ")
+else:
+    user=sys.argv[1]
+    passw=sys.argv[2]
+    video_url=sys.argv[3]
+    if len(sys.argv)==5:
+        directory=sys.argv[4]
+
+
+
+
+
+#Quick and dirty... horrible code to rewrite in the future
+print("Login on the teaching portal...")
+lcookie=polito_login(user,passw)
+print("Extracting video list...")
+links=extract_video_links(video_url,lcookie)
+print("\n"+str(len(links))+" videos found")
+print("Which videolessons do you want to download? Insert a range, right limit is excluded.")
+print("(For example to download all the videos give 1-"+str(len(links)+1)+")")
+inp=input("Range:")
+inp=re.match("(\d*)-(\d*)",inp)
+if inp:
+    st=int(inp.groups()[0])
+    end=int(inp.groups()[1])
+    for i in range(st,end):
+        download_video(links[i-1],lcookie,directory)
+else:
+    print("Wrong input")
+
+
+
+
+
+
+
+
+
