92155956eff5eb3bb06cabf9ff51e651eeea06a5
Giuseppe Lumia <glumia@protonmail.com>
2020-04-18 08:06:22 +0200

    Define more useful functions in scraping

--- polito_dl/scraping.py
+++ polito_dl/scraping.py
@@ -3,14 +3,6 @@ from polito_dl.parsing import get_course_data, parse_html, get_download_paths
 base_url = "https://didattica.polito.it"
 
 
-def direct_download_url(session, path):
-    if not path.startswith("/pls/portal30/sviluppo.videolezioni.download"):
-        raise ValueError("invalid download path")
-
-    resp = session.get(base_url + path, allow_redirects=False)
-    return resp.headers["Location"]
-
-
 def course_data(session, url):
     if not (
         url.startswith(base_url + "/portal/pls/portal/sviluppo.videolezioni.vis")
@@ -23,13 +15,19 @@ def course_data(session, url):
     return get_course_data(soup)
 
 
-def lecture_download_paths(session, url):
-    if not (
-        url.startswith(base_url + "/portal/pls/portal/sviluppo.videolezioni.vis")
-        or url.startswith(base_url + "/pls/portal30/sviluppo.videolezioni.vis")
-    ):
-        raise ValueError("invalid lecture URL")
+def download_paths(session, path):
+    if not path.startswith("sviluppo.videolezioni.vis"):
+        raise ValueError("invalid lecture path")
 
+    url = base_url + "/pls/portal30/" + path
     resp = session.get(url)
     soup = parse_html(resp.content)
     return get_download_paths(soup)
+
+
+def direct_download_url(session, path):
+    if not path.startswith("/pls/portal30/sviluppo.videolezioni.download"):
+        raise ValueError("invalid download path")
+
+    resp = session.get(base_url + path, allow_redirects=False)
+    return resp.headers["Location"]
--- tests/test_scraping.py
+++ tests/test_scraping.py
@@ -5,27 +5,10 @@ from requests import Session
 
 from polito_dl.scraping import (
     base_url,
-    direct_download_url,
     course_data,
-    lecture_download_paths,
+    download_paths,
+    direct_download_url,
 )
-
-
-def test_direct_download_url(requests_mock):
-    path = "/pls/portal30/sviluppo.videolezioni.download?fid=76424"
-    durl = "dummy_download_url"
-
-    requests_mock.get(base_url + path, status_code=302, headers={"Location": durl})
-
-    assert direct_download_url(Session(), path) == durl
-
-
-def test_direct_download_url_invalid_path():
-    invalid_path = "/pls/portal30/sviluppo.videolezioni.vis?cor=456"
-    session = Mock()
-
-    with pytest.raises(ValueError):
-        direct_download_url(session, invalid_path)
 
 
 @pytest.mark.parametrize(
@@ -71,17 +54,10 @@ def test_course_data_invalid_url():
         course_data(session, invalid_url)
 
 
-@pytest.mark.parametrize(
-    "path",
-    [
-        base_path + "/sviluppo.videolezioni.vis?cor=456&arg=Lezioni%20on-line&lez=20920"
-        for base_path in ("/portal/pls/portal", "/pls/portal30")
-    ],
-)
 @patch("polito_dl.scraping.get_download_paths")
 @patch("polito_dl.scraping.parse_html")
-def test_lecture_download_paths(parse_html, get_download_paths, path):
-    url = base_url + path
+def test_download_paths(parse_html, get_download_paths):
+    path = "sviluppo.videolezioni.vis?cor=456&arg=Lezioni%20on-line&lez=21927"
 
     html_content = "dummy_content"
     session = Mock()
@@ -95,20 +71,34 @@ def test_lecture_download_paths(parse_html, get_download_paths, path):
     expected_data = "dummy_data"
     get_download_paths.return_value = expected_data
 
-    data = lecture_download_paths(session, url)
+    data = download_paths(session, path)
 
-    session.get.assert_called_with(url)
+    session.get.assert_called_with(base_url + "/pls/portal30/" + path)
     parse_html.assert_called_with(html_content)
     get_download_paths.assert_called_with(soup)
     assert data == expected_data
 
 
-def test_lecture_download_paths_invalid_url():
-    invalid_url = (
-        "https://elearning.polito.it/courses/2011_PRE_MATE_ENG/document/"
-        "Videolezioni.html?cidReq=2011_PRE_MATE_ENG"
-    )
+def test_download_paths_invalid_path():
+    invalid_path = "Videolezioni.html?cidReq=2011_PRE_MATE_ENG"
     session = Mock()
 
     with pytest.raises(ValueError):
-        direct_download_url(session, invalid_url)
+        direct_download_url(session, invalid_path)
+
+
+def test_direct_download_url(requests_mock):
+    path = "/pls/portal30/sviluppo.videolezioni.download?fid=76424"
+    durl = "dummy_download_url"
+
+    requests_mock.get(base_url + path, status_code=302, headers={"Location": durl})
+
+    assert direct_download_url(Session(), path) == durl
+
+
+def test_direct_download_url_invalid_path():
+    invalid_path = "/pls/portal30/sviluppo.videolezioni.vis?cor=456"
+    session = Mock()
+
+    with pytest.raises(ValueError):
+        direct_download_url(session, invalid_path)
