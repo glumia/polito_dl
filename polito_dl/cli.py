13e8300074b0a737cf7bf6712fcefa6b981ae73b
Giuseppe Lumia <glumia@protonmail.com>
2020-04-18 09:24:26 +0200

    WIP CLI interface

--- polito_dl/cli.py
+++ polito_dl/cli.py
@@ -1,1 +1,38 @@
-# TODO: Click scripts for CLI
+from getpass import getpass
+
+import click
+
+from polito_dl import PolitoDownloader
+from polito_dl.auth import InvalidCredentials
+
+
+@click.command("polito_dl")
+@click.option("--username", type=str)
+@click.option("--password", type=str)
+@click.option("--course-data", default=False, help="Print course data and exit.")
+@click.option(
+    "--format",
+    default="video",
+    type=click.Choice(["video", "iphone", "audio"], case_sensitive=False),
+    help="",
+)
+@click.argument("url")
+def cli(username, password, course_data, format, url):
+    pdl = PolitoDownloader()
+    if not username:
+        username = input("Username: ")
+    if not password:
+        password = getpass("Password: ")
+
+    try:
+        pdl.login(username, password)
+    except InvalidCredentials:
+        click.echo("Login failed, check your username and password.")
+        return
+
+    if course_data:
+        pass
+
+
+if __name__ == "__main__":
+    cli()
